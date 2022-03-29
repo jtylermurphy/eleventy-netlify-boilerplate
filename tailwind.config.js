5a0e4881b20fb39a161302267e673e6bdc4919bd
jtylermurphy <jtylermurphy@gmail.com>
2022-03-29 16:05:47 -0500

    add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      sans: ['Lato', 'helvetica', 'arial', 'sans-serif'],
+      serif: ['Prata', 'serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
